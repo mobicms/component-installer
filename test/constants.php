91e1a4dd08d36a53ac0b7bb7a5fd7c9415c75102
Oleg Kasyanov <dev@mobicms.net>
2019-09-21 15:23:58 +0400

    Instead mobicms/dummy used local test constants

---
+++ test/constants.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of mobiCMS Content Management System.
+ *
+ * @copyright   Oleg Kasyanov <dev@mobicms.net>
+ * @license     https://opensource.org/licenses/GPL-3.0 GPL-3.0 (see the LICENSE.md file)
+ * @link        http://mobicms.org mobiCMS Project
+ */
+
+if (!defined('CMS_PATH_CONFIG')) {
+    define('CMS_PATH_CONFIG', __DIR__);
+}

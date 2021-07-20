bb6507733c454b1f85adefd03af69c1094b991b1
Oleg Kasyanov <oleg@batumi.org>
2021-07-21 01:24:34 +0400

    Removed file headers

--- tests/resources/stubs/constants.php
+++ tests/resources/stubs/constants.php
@@ -2,14 +2,6 @@
 
 declare(strict_types=1);
 
-/*
- * This file is part of mobiCMS Content Management System.
- *
- * @copyright   Oleg Kasyanov <dev@mobicms.net>
- * @license     https://opensource.org/licenses/GPL-3.0 GPL-3.0 (see the LICENSE.md file)
- * @link        http://mobicms.org mobiCMS Project
- */
-
 if (! defined('M_PATH_CONFIG')) {
     define('M_PATH_CONFIG', __DIR__);
 }
--- tests/unit/InstallHandlers/RoutesConfigHandlerTest.php
+++ tests/unit/InstallHandlers/RoutesConfigHandlerTest.php
@@ -2,14 +2,6 @@
 
 declare(strict_types=1);
 
-/*
- * This file is part of mobiCMS Content Management System.
- *
- * @copyright   Oleg Kasyanov <dev@mobicms.net>
- * @license     https://opensource.org/licenses/GPL-3.0 GPL-3.0 (see the LICENSE.md file)
- * @link        http://mobicms.org mobiCMS Project
- */
-
 namespace MobicmsTest\ComponentInstaller\InstallHandlers;
 
 use Composer\Installer\LibraryInstaller;
